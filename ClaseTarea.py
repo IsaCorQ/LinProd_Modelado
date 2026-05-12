076e629489b710b6bbf0ae4a6c1feb427577bfd1
IsaCorQ <85208919+IsaCorQ@users.noreply.github.com>
2026-05-12 14:29:13 -0600

    Arreglos a clase tarea

--- ClaseTarea.py
+++ ClaseTarea.py
@@ -1,30 +1,46 @@
 from queue import Queue
 
 class Tarea:
-    def __init__(self,idn,nombre,tiempo):
+    def __init__(self, idn, nombre, tiempo_proceso):
+
         self.idn = idn
         self.nombre = nombre
-        self.tiempoProcesamiento = tiempo
-        self.tiempoRestante = tiempo
-        self.ocupada = False
-        self.colaEspera = Queue() #Cola de productos, hay que crear clase
-        self.productoAnual = "productoAnual" #Ligar a clase producto
-
-    def iniciar_proceso(self, producto): #help
-        print(f"iniciar proceso")
-    
-    def avanzar_ciclo(self):
-        self.tiempoRestante = self.tiempoRestante-1 #checar esto
-
-    def finalizar_proceso(self): #help
-        print(f"finalizar proceso")
-        proceso = self.colaEspera.get()
-        return proceso
-
-    def esta_disponible(self):
-        print(f"finalizar proceso")
-        return self.ocupada
+        self.tiempo_proceso = tiempo_proceso  # TP
+        self.tiempo_restante = 0
+        self.esta_procesando = False  # EP
+        self.contenido_esperando = Queue()  # CE
+        self.producto_actual = None
 
     def agregar_cola(self, producto):
-        self.colaEspera.put(producto)
+        self.contenido_esperando.put(producto)
+
+    def iniciar_proceso(self, producto):
+        if not self.esta_procesando and not self.contenido_esperando.empty():
+            self.producto_actual = self.contenido_esperando.get()
+            self.esta_procesando = True
+            self.tiempo_restante = self.tiempo_proceso
+            return True
+        return False
+
+    def avanzar_ciclo(self):
+        if self.esta_procesando:
+            self.tiempo_restante -= 1
+            if self.tiempo_restante <= 0:
+                return self.finalizar_proceso()
+        return None
+
+    def finalizar_proceso(self):
+        if self.esta_procesando:
+            producto_completado = self.producto_actual
+            self.producto_actual = None
+            self.esta_procesando = False
+            self.tiempo_restante = 0
+            return producto_completado
+        return None
+
+    def esta_disponible(self):
+        return not self.esta_procesando
+
+    def obtener_tamaño_cola(self):
+        return self.contenido_esperando.qsize()
         
